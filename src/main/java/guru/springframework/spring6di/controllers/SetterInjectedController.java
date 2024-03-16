791bf20159ca45fca4df1b84fde170aa64c2611e
Jörg-Christian Müller <joerg-christian.mueller@shv-is.eu>
2024-03-16 13:53:59 +0100

    The Setter-Method has to be annotated not the property.

--- src/main/java/guru/springframework/spring6di/controllers/SetterInjectedController.java
+++ src/main/java/guru/springframework/spring6di/controllers/SetterInjectedController.java
@@ -11,11 +11,11 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class SetterInjectedController {
 
-    @Qualifier("setterGreetingBean")
-    @Autowired
     private GreetingService greetingService;
 
 
+    @Qualifier("setterGreetingBean")
+    @Autowired
     public void setGreetingService(@Qualifier("setterGreetingBean") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
